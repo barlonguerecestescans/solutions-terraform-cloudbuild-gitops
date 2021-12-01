9a0b6d5f7ebaf95114d85dd6a0c3ce85bda851e7
Pierre <barlonguereceterscans@gmail.com>
2021-12-01 14:18:12 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="molten-nirvana-333116"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "molten-nirvana-333116-tfstate"
     prefix = "env/prod"
   }
 }
